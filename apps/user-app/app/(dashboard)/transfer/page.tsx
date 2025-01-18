d616e591a40377355396eb6a140a508af872ccee
tejas <pangaonkar.svc424@gmail.com>
2025-01-18 21:01:39 +0530

    added ts types to transfer

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -5,6 +5,13 @@ import { getServerSession } from "next-auth";
 import prisma from "@repo/db/client";
 import { authOptions } from "../../lib/auth";
 
+interface Transaction {
+    startTime: Date;
+    amount: number;
+    status: string;
+    provider: string;
+}
+
 async function getBalance() {
     const session = await getServerSession(authOptions);
     const balance = await prisma.balance.findFirst({
@@ -25,7 +32,7 @@ async function getOnRampTransactions() {
             userId: Number(session?.user?.id)
         }
     });
-    return txns.map(t => ({
+    return txns.map((t : Transaction) => ({
         time: t.startTime,
         amount: t.amount,
         status: t.status,
